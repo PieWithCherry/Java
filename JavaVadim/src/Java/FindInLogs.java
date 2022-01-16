8742dd1c2d5c0498ae1644453bcc6689a86e8715
PieWithCherry <75637443+PieWithCherry@users.noreply.github.com>
2022-01-17 01:22:06 +0300

    Add files via upload

---
+++ JavaVadim/src/Java/FindInLogs.java
@@ -0,0 +1,28 @@
+package Java;
+
+import Java.utils.Logs;
+
+/**
+ * Консольное приложение, с помощью которого можно найти по регулярному выражению
+ * находит строки в файле/файлах и заносит их в отдельный файл
+ * <p>
+ * args[0] - регулярное выражение (пробелы в регулярном выражении не допускаются)
+ * args[1] - путь до файла/файлов (если директория - то в конце обязательно \\)
+ * args[2] - название нового файла
+ */
+public class FindInLogs {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Не хватает аргументов");
+        } else if (args.length > 3) {
+            throw new IllegalArgumentException("Аргументов слишком много " +
+                    "(пробелы в регулярном выражении не допускаются, только как \\s)");
+        }
+        String regExp = args[0];
+        String path = args[1];
+        String newName = args[2];
+
+        Logs logs = new Logs(path);
+        logs.findInFiles(regExp, newName);
+    }
+}
---
+++ JavaVadim/src/Java/SplitFiles.java
@@ -0,0 +1,24 @@
+package Java;
+
+import Java.utils.Logs;
+
+/**
+ * Консольное приложение, позволяющее разделить один файл на несколько
+ * новые файлы помещаются в отдельную папку,
+ * которая находится в той же директории, что и программа
+ * <p>
+ * arg[0] - путь до файла, который мы хотим разделить
+ * arg[1] - строка с основным именем новых файлов (name_001, name_002 итд)
+ * оно же и является именем папки (\\ + args[1] + \\)
+ */
+public class SplitFiles {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Не хватает аргументов");
+        }
+        String nameBigFile = args[0];
+        String nameNewFile = args[1];
+
+        Logs logs = new Logs(nameBigFile, nameNewFile);
+    }
+}
